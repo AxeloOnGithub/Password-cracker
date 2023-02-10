acefbb360bda7fe9655b79ec755a79816e31239f
AxeloFromGithub <aksel@kirchert.dk>
2023-02-10 16:28:05 +0100

    Added settings loading

--- crack.py
+++ crack.py
@@ -1,10 +1,20 @@
+import json
+
+# loads settings.json
+
+cracked = ""
+
+with open("settings.json", "r") as f:
+    json_object = json.loads(f.read())
+    lenght = json_object["lenght"]
+
+for i in range(0,lenght):
+    cracked = cracked + "1"
+
+###################################
 
 characters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
               "U", "V", "W", "X", "Y", "Z", "^", "_", "`", "!", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"]
-
-cracked = "1"
-
-sep = ""
 
 def crack():
 
